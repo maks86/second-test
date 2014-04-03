fe0e7c9b04b01f42fd13b9ca09cc8c2ad0f20de5
Maks Shkvirya <Maks Shkvirya>
2014-04-04 00:27:00 +0300

    First commit maks

---
+++ src/com/vk/Test.java
@@ -0,0 +1,7 @@
+package com.vk;
+
+/**
+ * Created by 1 on 04.04.14.
+ */
+public class Test {
+}
---
+++ src/com/vk/model/Maks.java
@@ -0,0 +1,7 @@
+package com.vk.model;
+
+/**
+ * Created by 1 on 04.04.14.
+ */
+public class Maks {
+}
